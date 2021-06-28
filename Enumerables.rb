addde1d591cb6021dfce5dccf8bed0b1d0a1f17c
Sydney <sydneyparsonsyb@gmail.com>
2021-06-28 17:27:47 -0400

    third

--- Enumerables.rb
+++ Enumerables.rb
@@ -36,13 +36,52 @@ class Array
     end
 
     def my_any?(&block)
-        
-
+        self.my_each do |ele|
+            if block.call(ele) == true
+                return true
+            end
+        end
+        false
     end
+
+    def my_all?(&block)
+        self.my_each do |ele|
+            if !block.call(ele)
+                return false
+            end
+        end
+        true
+    end
+    # require 'byebug'    
+    def my_flatten
+        # debugger
+        # return [self] unless self.is_a?(Array)
+        # debugger
+        flattened = []
+        # debugger
+        self.each do |ele|
+            if ele.is_a?(Array)
+            # debugger
+                flattened += ele.my_flatten
+            # debugger
+            else
+                flattened << ele
+            end
+        end
+        # debugger
+        flattened
+    end
+
 end
 
+# p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
 
+# a = [1, 2, 3]
+# p a.my_any? { |num| num > 1 } # => true
+# p a.my_any? { |num| num == 4 } # => false
+# p a.my_all? { |num| num > 1 } # => false
+# p a.my_all? { |num| num < 4 } # => true
 
-a = [1, 2, 3]
-p a.my_reject { |num| num > 1 } # => [1]
-p a.my_reject { |num| num == 4 } # => [1, 2, 3]
+# a = [1, 2, 3]
+# p a.my_reject { |num| num > 1 } # => [1]
+# p a.my_reject { |num| num == 4 } # => [1, 2, 3]
