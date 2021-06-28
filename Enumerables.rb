a145d76a5a69062222a5bf81425648d1b0e94885
Sunny Mei <sunmeiappprep@gmail.com>
2021-06-28 16:24:50 -0400

    2nd

--- Enumerables.rb
+++ Enumerables.rb
@@ -1,1 +1,48 @@
-p 'testing'
+class Array
+
+    def my_each(&block)
+        i = 0
+
+        while i < self.length
+            block.call(self[i])
+            i += 1
+        end
+        self
+    end
+
+    def my_select(&block)
+        new_arr = []
+
+        self.my_each do |ele|
+            if block.call(ele) == true
+                new_arr << ele
+            end
+        end
+
+        new_arr
+
+    end
+
+    def my_reject(&block)
+        new_arr = []
+
+        self.my_each do |ele|
+            if block.call(ele) == false
+                new_arr << ele
+            end
+        end
+
+        new_arr
+    end
+
+    def my_any?(&block)
+        
+
+    end
+end
+
+
+
+a = [1, 2, 3]
+p a.my_reject { |num| num > 1 } # => [1]
+p a.my_reject { |num| num == 4 } # => [1, 2, 3]
